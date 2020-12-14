f27fd277fefce26e5154ba55e9d49dafcd855c46
Dianne <dianne.diazm@gmail.com>
2020-12-14 13:19:54 -0300

    avance home

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,84 +1,100 @@
 <template>
-  <div class="background">
-    <h1>Inicia sesión</h1>
-    <el-form :model="dynamicValidateForm" ref="dynamicValidateForm" label-width="120px" class="demo-dynamic">
-      <el-form-item
-        prop="email"
-        label="Email"
-        :rules="[
-          { required: true, message: 'Please input email address', trigger: 'blur' },
-          { type: 'email', message: 'Please input correct email address', trigger: ['blur', 'change'] }
-        ]"
-      >
-        <el-input v-model="dynamicValidateForm.email"></el-input>
-      </el-form-item>
-      <el-form-item
-        v-for="(domain, index) in dynamicValidateForm.domains"
-        :label="'Password'"
-        :key="domain.key"
-        :prop="'domains.' + index + '.value'"
-        :rules="{
-          required: true, message: 'domain can not be null', trigger: 'blur'
-        }"
-      >
-        <el-input v-model="domain.value"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('dynamicValidateForm')">Login</el-button>
-        <el-button @click="resetForm('dynamicValidateForm')">Reset</el-button>
-      </el-form-item>
-    </el-form>
-  </div>
+  <div class="container my-5">
+    <img src="../assets/img/proyecto.png" class="image-home mr-5" alt="">
+        <b-form @submit.prevent="login" class="form">
+          <h4>Inicia sesión</h4>
+        <b-form-group
+            id="input-group-1"
+            label="Correo electrónico"
+            label-for="input-1"
+        >
+            <b-form-input
+            id="input-1"
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="Escribe tu correo electrónico"
+            ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Contraseña" label-for="input-2">
+            <b-form-input
+            id="input-2"
+            v-model="form.password"
+            type="password"
+            required
+            placeholder="Escribe tu contraseña"
+            ></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Iniciar sesión</b-button>
+        <b-button type="reset" variant="danger" class="ml-3">Reset</b-button>
+        </b-form>
+        
+    </div>
 </template>
 
 <script>
+import firebase from 'firebase';
 export default {
-  name: 'Inicio',
-  data() {
+    name: 'Inicio',
+    data() {
       return {
-        dynamicValidateForm: {
-          domains: [{
-            key: 1,
-            value: ''
-          }],
-          email: ''
-        }
-      };
+        form: {
+          email: '',
+          password: '',
+        },
+        show: true
+      }
     },
     methods: {
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('submit!');
-          } else {
-            console.log('error submit!!');
-            return false;
+      login(){
+          if (this.form.email && this.form.password && this.form.password.length >= 6){
+              console.log("ingresa");
+              firebase.auth().signInWithEmailAndPassword(this.form.email, this.form.password)
+              .then(resp => {
+                  console.log(resp.user);
+                  this.$router.push('/user');
+              })
+              .catch(error => {
+                  console.error(error.code);
+                  console.error(error.message);
+                if (error.code == "auth/wrong-password") {
+                    this.$notify.error({
+                    title: 'Error',
+                    message: 'Contraseña inválida'
+                });
+                } else if (error.code == "auth/user-not-found") {
+                    this.$notify.error({
+                    title: 'Error',
+                    message: 'Usuario no encontrado'
+                });
+                this.$router.push('/registro')
+                }                   
+              })
           }
-        });
-      },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      },
-      removeDomain(item) {
-        var index = this.dynamicValidateForm.domains.indexOf(item);
-        if (index !== -1) {
-          this.dynamicValidateForm.domains.splice(index, 1);
-        }
-      },
-      addDomain() {
-        this.dynamicValidateForm.domains.push({
-          key: Date.now(),
-          value: ''
-        });
+          else {
+              console.log("No ingresa");
+          }
       }
-    }
-}
+    },
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-.background {
-  background-color: #fcf9ea;
+
+.image-home {
+  width: 50%;
+}
+
+.container {
+  display: flex;
+}
+
+.form {
+  width: 100%;
+  margin-top: 110px;
 }
 
 </style>

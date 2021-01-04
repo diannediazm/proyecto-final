d20edac6b0c6df31b1d7fab741f7695f085d3ac3
Claudio Díaz <claudio.diaz.morales@gmail.com>
2021-01-03 22:07:26 -0300

    about con querys para mostrar elementos de base de datos

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,26 +1,99 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <input v-rut v-model="rut">
+    <h1>historias</h1>
+    <div v-for="(data, index) in historia" :key="index">
+      <p>Titulo: {{ historia[index].title }}</p>
+      <p>Text: {{ historia[index].text }}</p>
+    </div>
+    <h1>videos</h1>
+    <div v-for="(data, index) in video" :key="index">
+      <p>Nombre: {{ video[index].name }}</p>
+      <p>Descripcion: {{ video[index].desc }}</p>
+      <p>fecha: {{ video[index].date1 }}</p>
+      <p>videoURL: {{ video[index].videoURL }}</p>
+      <b-embed
+        type="iframe"
+        aspect="16by9"
+        :src= video[index].videoURL 
+        allowfullscreen
+      ></b-embed>
+    </div>
 
+    <h1>fotos</h1>
+    <div v-for="(data, index) in foto" :key="index">
+      <p>Nombre: {{ foto[index].name }}</p>
+      <p>Desc: {{ foto[index].desc }}</p>
+      <p>fecha foto: {{ foto[index].date1 }}</p>
+      <b-img :src= foto[index].photoURL></b-img>
+    </div>
 
   </div>
 </template>
 
 <script>
+import { db } from "../main";
 
 export default {
-  name: 'About',
+  name: "About",
   data() {
     return {
-      rut: ''
-      }
-
+      historia: [],
+      video: [],
+      foto:[],
+    };
   },
 
- beforeCreate: function(){
-      console.log("about: " + this.$store.state.userSession);
-    }
- 
-}
+  beforeCreate: function () {
+    db.collection("videos")
+      .where("userID", "==", this.$store.state.userSession)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          console.log("No matching documents.");
+          return;
+        }
+        snapshot.forEach((doc) => {
+          this.video.push(doc.data());
+        });
+      })
+      .catch((err) => {
+        console.log("Error getting documents", err);
+      });
+  },
+
+  mounted(){
+      db.collection("fotos")
+      .where("userID", "==", this.$store.state.userSession)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          console.log("No matching documents.");
+          return;
+        }
+        snapshot.forEach((doc) => {
+          this.foto.push(doc.data());
+        });
+      })
+      .catch((err) => {
+        console.log("Error getting documents", err);
+      });
+  },
+  created() {
+    db.collection("historias")
+      .where("idUser", "==", this.$store.state.userSession)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          console.log("No matching documents.");
+          return;
+        }
+        snapshot.forEach((doc) => {
+          this.historia.push(doc.data());
+        });
+      })
+      .catch((err) => {
+        console.log("Error getting documents", err);
+      });
+  },
+};
 </script>

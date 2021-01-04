6c919abd73c5d6c8b444fc8859d5662adaf9b61e
Claudio Díaz <claudio.diaz.morales@gmail.com>
2021-01-03 23:09:27 -0300

    nombre barra menu

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
 
     <b-navbar toggleable="lg" type="dark" variant="dark">
-      <b-navbar-brand :to="{name: 'Home'}">HUKO</b-navbar-brand>
+      <b-navbar-brand :to="{name: 'Home'}">HOKU</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 

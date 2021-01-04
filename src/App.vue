95e015e13e875acae5de28b88ff26c927bd1bf31
Claudio Díaz <claudio.diaz.morales@gmail.com>
2021-01-04 13:08:11 -0300

    menu listo

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,141 @@
 <template>
-  <div>
+     <div>
+          <b-navbar toggleable="lg" type="dark" variant="dark">
+               <b-navbar-brand :to="{ name: 'Home' }">HOKU</b-navbar-brand>
 
-    <b-navbar toggleable="lg" type="dark" variant="dark">
-      <b-navbar-brand :to="{name: 'Home'}">HOKU</b-navbar-brand>
+               <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item v-if="!uid" href="/#login">Iniciar Sesión</b-nav-item>
-          <b-nav-item v-if="!uid" :to="{ name: 'Registro' }">Crear Cuenta</b-nav-item>
-          <b-nav-item v-if="uid" :to="{name: 'Usuario'}"><b-icon icon="house-door"></b-icon> Inicio</b-nav-item>
-          <b-nav-item v-if="uid" :to="{name: 'Historias'}"><b-icon icon="plus-circle"></b-icon> Historias</b-nav-item>
-          <b-nav-item v-if="uid" :to="{name: 'Fotos'}"><b-icon icon="plus-circle"></b-icon> Fotos</b-nav-item>
-          <b-nav-item v-if="uid" :to="{name: 'Videos'}"><b-icon icon="plus-circle"></b-icon> Videos</b-nav-item>
-          <!-- <b-nav-item v-if="uid" :to="{name: 'About'}">About</b-nav-item> -->
-          <b-nav-item v-if="uid" href="#" @click="salir">Salir</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-    <!-- <b-navbar toggleable type="dark" variant="dark">
-            <b-navbar-brand href="/">HUKO</b-navbar-brand>
-
-            <b-navbar-toggle target="navbar-toggle-collapse">
-            <template #default="{ expanded }">
-                <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
-                <b-icon v-else icon="chevron-bar-down"></b-icon>
-            </template>
-            </b-navbar-toggle>
-
-            <b-collapse id="navbar-toggle-collapse" is-nav>
-            <b-navbar-nav class="mr-auto">
-                <b-nav-item href="#">Link 1</b-nav-item>
-                <b-nav-item href="#">Link 2</b-nav-item>
-                <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            </b-navbar-nav>
-            </b-collapse>
-        </b-navbar> -->
-     
-    <router-view></router-view>
-  </div>
+               <b-collapse id="nav-collapse" is-nav>
+                    <b-navbar-nav class="ml-auto">
+                         <b-nav-item v-if="!uid" href="/#login"
+                              >Iniciar Sesión</b-nav-item
+                         >
+                         <b-nav-item v-if="!uid" :to="{ name: 'Registro' }"
+                              >Crear Cuenta</b-nav-item
+                         >
+                         <b-nav-item
+                              class="my-auto"
+                              v-if="uid"
+                              :to="{ name: 'Usuario' }"
+                              ><b-icon icon="house-door"></b-icon>
+                              Inicio</b-nav-item
+                         >
+                         <b-nav-item
+                              class="my-auto"
+                              v-if="uid"
+                              :to="{ name: 'Historias' }"
+                              ><b-icon icon="plus-circle"></b-icon>
+                              Historias</b-nav-item
+                         >
+                         <b-nav-item
+                              class="my-auto"
+                              v-if="uid"
+                              :to="{ name: 'Fotos' }"
+                              ><b-icon icon="plus-circle"></b-icon>
+                              Fotos</b-nav-item
+                         >
+                         <b-nav-item
+                              class="my-auto"
+                              v-if="uid"
+                              :to="{ name: 'Videos' }"
+                              ><b-icon icon="plus-circle"></b-icon>
+                              Videos</b-nav-item
+                         >
+                         <!-- <b-nav-item v-if="uid" :to="{name: 'About'}">About</b-nav-item> -->
+                         <b-nav-item-dropdown v-if="uid" class="my-auto" right>
+                              <template #button-content>
+                                   <span class="my-auto">
+                                     Hola {{ usuario[0].primerNombre }}
+                                   </span>
+                                   <b-img
+                                        v-bind="mainProps"
+                                        :src="usuario[0].photoPerfil"
+                                        rounded="circle"
+                                        alt="Circle image"
+                                   ></b-img>
+                              </template>
+                              <b-dropdown-item href="#"
+                                   ><b-icon icon="pencil-square"></b-icon>
+                                   Editar Perfil</b-dropdown-item
+                              >
+                              <b-dropdown-item href="#" @click="salir"
+                                   ><b-icon icon="x-circle"></b-icon> Cerrar
+                                   Sesión</b-dropdown-item
+                              >
+                         </b-nav-item-dropdown>
+                    </b-navbar-nav>
+               </b-collapse>
+          </b-navbar>
+          <router-view></router-view>
+     </div>
 </template>
 
 <script>
 import firebase from "firebase";
+import { db } from "./main";
 
 export default {
-  name: 'App',
-  data() {
-    return {
-      uid: "",
-    };
-  },
-  methods: {
-    salir(){
-        firebase.auth().signOut().then(() => {
-            this.$router.replace('/');
-        }).catch((error) => {
-            console.error(error);
-        });
-      }
-  },
+     name: "App",
+     data() {
+          return {
+               uid: "",
+               usuario: [],
+               mainProps: {
+                    // blank: true,
+                    // blankColor: "#777",
+                    width: 40,
+                    height: 40,
+               },
+          };
+     },
+     methods: {
+          salir() {
+               firebase
+                    .auth()
+                    .signOut()
+                    .then(() => {
+                         this.usuario = "";
+                         this.$router.replace("/");
+                    })
+                    .catch((error) => {
+                         console.error(error);
+                    });
+          },
+     },
 
- mounted() {
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        //console.log(user.displayName);
-        this.uid = user.uid;
-        this.$store.dispatch("updateUserSessionAct", user.uid);
-        // console.log("Si hay usuario con sesión activa");
-      } else {
-        //  console.log("No hay usuario con sesión activa...");
-        this.uid = "";
-        let sesionUsuario = null
-        this.$store.dispatch("updateUserSessionAct", sesionUsuario);
-      }
-    });
-  },
-  
-}
+     mounted() {
+          firebase.auth().onAuthStateChanged((user) => {
+               if (user) {
+                    //console.log(user.displayName);
+                    setTimeout(() => {
+                         this.uid = user.uid;
+                         this.$store.dispatch("updateUserSessionAct", user.uid);
+                         db.collection("usuarios")
+                              .where("userID", "==", user.uid)
+                              .get()
+                              .then((snapshot) => {
+                                   if (snapshot.empty) {
+                                        console.log("No matching documents.");
+                                        return;
+                                   }
+                                   snapshot.forEach((doc) => {
+                                        this.usuario.push(doc.data());
+                                   });
+                              })
+                              .catch((err) => {
+                                   console.log("Error getting documents", err);
+                              });
+                    }, 1500);
+                    // console.log("Si hay usuario con sesión activa");
+               } else {
+                    //  console.log("No hay usuario con sesión activa...");
+                    this.uid = "";
+                    let sesionUsuario = null;
+                    this.$store.dispatch("updateUserSessionAct", sesionUsuario);
+               }
+          });
+     },
+};
 </script>
 
 <style lang="scss">
